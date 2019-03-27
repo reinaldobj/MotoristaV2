f3090b92f6c812cf7db7119f2a4c1e85e4e9cbbc
Reinaldo Bispo Pereira Junior <reinaldo.j@gmail.com>
2019-03-27 10:18:46 -0300

    Correção para deixar o filtro opcional

--- Motorista.WebApi/Requests/Motorista/MotoristaGetListRequest.cs
+++ Motorista.WebApi/Requests/Motorista/MotoristaGetListRequest.cs
@@ -11,7 +11,7 @@ namespace Motorista.WebApi.Requests.Motorista
         /// <summary>
         /// Nome do motorista
         /// </summary>
-        [FromRoute(Name = "nome")]
+        [FromQuery(Name = "nome")]
         public string Nome { get; set; }
     }
 }
